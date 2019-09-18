1684c62a3524db8cc1ce04e312bc7f0fd3d8b225
zhulu <shaonianzhentan@sina.cn>
2019-09-18 14:44:50 +0800

    初始化项目

---
+++ dist/speak.js
@@ -0,0 +1,35 @@
+class CardSpeak extends HTMLElement {
+
+  constructor() {
+    super()
+    this.attachShadow({ mode: 'open' })
+  }
+
+  set hass(hass) {
+ 
+  }
+
+  setConfig(config) {    
+    this.config = config;
+    let cardContainer = document.createElement('ha-card')
+    let cardContainerStyle = document.createElement('style')
+    // cardContainerStyle.textContent = `
+    //   ha-card {
+    //     padding: calc(${config.padding} / 2);
+    //     display: flex;
+    //     justify-content: space-around;
+    //     flex-wrap: wrap;
+    //     ${cardStyle}
+    //   }
+    // `
+
+    this.shadowRoot.appendChild(cardContainer)
+    this.shadowRoot.appendChild(cardContainerStyle)
+  }
+
+  getCardSize() {
+    return 1;
+  }
+}
+
+customElements.define('card-speak', CardSpeak);
